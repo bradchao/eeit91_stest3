3d597def605270a9f49eba2aa5e2fb4b6ae937c4
User <User@B01.r1805.tciii.org.tw>
2024-12-24 08:51:50 +0800

    init

---
+++ stest3/src/main/java/tw/brad/stest3/Stest3Application.java
@@ -0,0 +1,13 @@
+package tw.brad.stest3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stest3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stest3Application.class, args);
+	}
+
+}
---
+++ stest3/src/test/java/tw/brad/stest3/Stest3ApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.brad.stest3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stest3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
